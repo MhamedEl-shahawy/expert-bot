214a92dbad9683888d1772fe6449b47678767a47
MhamedElshahawy <mohamedelshahawy599@gmail.com>
2022-04-06 23:44:05 +0200

    Updates

---
+++ precache-manifest.2c5fc59ef2bb868f9d7389c760b6e439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6069319e9e207fb16ab8d29961c5df57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "772df43b298cbe2555e6",
+    "url": "./static/css/main.d620d7e6.chunk.css"
+  },
+  {
+    "revision": "c7db3b6898e2dd64408f",
+    "url": "./static/js/2.2a4b64c7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2a4b64c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772df43b298cbe2555e6",
+    "url": "./static/js/main.3daa8782.chunk.js"
+  },
+  {
+    "revision": "6a14280648f164fac7ce",
+    "url": "./static/js/runtime-main.9be3931b.js"
+  }
+]);
